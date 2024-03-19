09d4baeaa77cf846834566613ee9f72e6cf8ac59
vsa15 <valencioaraujo15@gmail.com>
2024-03-19 13:27:41 +0530

    Add and Delete Functionality Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_application/util/dialogue_box.dart';
 import 'package:todo_application/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +10,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //text Controller
+  final _controller = TextEditingController();
+
+  //Task List
   List toDoList = [
     ["Make List Data", false],
     ["Do Exercise", false],
@@ -23,21 +28,54 @@ class _HomePageState extends State<HomePage> {
         title: const Text('TO DO APPLICATION'),
         elevation: 0,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: const Icon(Icons.add),
+      ),
       body: ListView.builder(
         itemCount: toDoList.length,
         itemBuilder: (context, index) {
           return ToDoTile(
-              taskName: toDoList[index][0],
-              taskCompleted: toDoList[index][1],
-              onChanged: (value) => checkBoxChanged(value!, index));
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value!, index),
+            deleteFunction: (context) => deleteTask(index),
+          );
         },
       ),
+    );
+  }
+
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onCancel: () => Navigator.of(context).pop(),
+          onSave: saveNewTask,
+        );
+      },
     );
   }
 
   void checkBoxChanged(bool? value, int index) {
     setState(() {
       toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+    });
+    print(toDoList);
+    Navigator.of(context).pop();
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
     });
   }
 }
---
+++ lib/util/buttons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomButtoms extends StatelessWidget {
+  final String text;
+  VoidCallback onPressed;
+
+  CustomButtoms({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color:  Color.fromARGB(100, 238, 159, 253),
+      child: Text(text),
+    );
+  }
+}
